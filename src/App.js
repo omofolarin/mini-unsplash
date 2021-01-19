617b857303d225c9ea79b2fbed697c8cb8cd6dba
omofolarin <folarinshonibare@yahoo.com>
2021-01-19 12:30:37 +0100

    quick start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Search as SearchIcon } from "react-feather";
+
+const Hero = () => {
+  const rootStyle = {
+    width: "100%",
+    display: "flex",
+    position: "relative",
+    height: "25em",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#CCC",
+  };
+  return (
+    <div style={rootStyle}>
+      <Container>
+        <SearchInput />
+      </Container>
+    </div>
+  );
+};
+
+const Container = (props) => {
+  const { children, maxWidth } = props;
+  const rootStyle = {
+    maxWidth: "980px",
+    width: "100%",
+  };
+  return <div style={rootStyle}>{children}</div>;
+};
+
+const SearchInput = () => {
+  const rootStyle = {
+    width: "80%",
+    margin: "0 auto",
+    height: "3em",
+    backgroundColor: "#fff",
+    border: "1px solid transparent",
+    borderRadius: "5px",
+    display: "flex",
+    alignItems: "center",
+    padding: "0 16px",
+  };
+
+  const searchIcon = {
+    color: "#767676",
+  };
+
+  const inputStyle = {
+    width: "100%",
+    flexGrow: 1,
+    display: "flex",
+    height: "100%",
+    border: "none",
+    outline: "none",
+    fontSize: "20px",
+    borderShadow: "none",
+  };
+
+  const inputContainerStyle = {
+    flex: 1,
+    position: "relative",
+    width: "100%",
+    display: "flex",
+    paddingLeft: "16px",
+    height: "3em",
+  };
+
+  return (
+    <form style={rootStyle}>
+      <div role="button">
+        <SearchIcon style={searchIcon} />
+      </div>
+      <div style={inputContainerStyle}>
+        <input
+          type="search"
+          autocomplete="off"
+          style={inputStyle}
+          arialAutoComplete="search"
+          placeholder="Search for photo"
+        />
+      </div>
+    </form>
+  );
+};
+
+const ImageThumbnail = () => {
+  return null;
+};
+
+const Modal = () => {};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hero />
     </div>
   );
 }
